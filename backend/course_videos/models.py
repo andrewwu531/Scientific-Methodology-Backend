4c3f81f0ff0625f2992dee856a9f492852613659
unknown <andrewwu0531>
2024-05-25 15:53:54 +0100

    updated media files

--- backend/course_videos/models.py
+++ backend/course_videos/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     course_title = models.CharField(max_length=100, default="default_title")
     course_image = models.ImageField(upload_to='course_image', null=True, default='course_image/Muscle_Building_Image_1.jpg')
     course_sub_image = models.ImageField(upload_to='course_sub_image', null=True, default='course_image/Muscle_Building_Image_3.jpg')
-    course_video = models.FileField(upload_to='course_video', null=True, default='course_image/Muscle_Building_Video_1.mp4')
+    course_video = models.FileField(upload_to='course_video', null=True, default='course_video/Muscle_Building_Video_1.mp4')
 
     def _str_(self):
         return self.course_name
