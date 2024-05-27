5336bde2836e2e203f2cfdd8d8c00038401f9d0c
unknown <andrewwu0531>
2024-05-27 17:41:13 +0100

    completed part of the muscle building course page

--- frontend/src/components/MainContents/MuscleBuilding.jsx
+++ frontend/src/components/MainContents/MuscleBuilding.jsx
@@ -1,15 +1,27 @@
 import mb_portrait from "../../static/images/muscle_building_portrait.png";
+import mb_banner from "../../static/images/course_banner.png";
 import Footer from "../../components/Footer/Footer";
 
 export default function MuscleBuilding() {
   return (
-    <div className="flex flex-col overflow-auto h-[87vh] scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-800 scrollbar-thumb-hover-gray-600">
+    <div className="flex flex-col pt-8 pl-8 overflow-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-800 scrollbar-thumb-hover-gray-600">
 
-      <div className="my-6 ml-8 text-lg font-bold">
+      <div className="pb-2 text-2xl font-bold">
         The Science of Shredded Muscle Building
       </div>
 
-      <img src={ mb_portrait } alt="portrait" className='object-cover w-48 h-48 my-6 ml-12 rounded-full'/>
+      <div className="flex justify-start pl-5">
+        <div className="relative pt-5 w-36 h-38">
+          <img src={ mb_portrait } alt="portrait" className='object-cover w-full h-full rounded-full'/>
+        </div>
+        <div className="h-40 pt-5 pl-10 w-280">
+          <img src={ mb_banner } alt="banner" className='object-cover w-full h-full'/>
+        </div>
+      </div>
+
+      <div className="pl-5 text-lg font-bold pt-7">
+        Series 1: Nutrition Theory
+      </div>
 
       
       <Footer />
--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -11,11 +11,11 @@ export default function Root() {
       <div>
         <Navbar />
       </div>
-      <div className="h-[91vh] min-w-[50rem] grid overflow-hidden grid-cols-[min-content_auto] gap-y-2 p-2 bg-black-2">
+      <div className="relative z-10 h-[91vh] min-w-[50rem] grid overflow-hidden grid-cols-[min-content_auto] gap-y-2 p-2 bg-black-2">
         <Sidebar />
         
 
-        <SectionContainer className="relative overflow-auto bg-black-1">
+        <SectionContainer className="relative overflow-auto z-25 bg-black-1">
 
 
           <MainContents />
@@ -26,7 +26,7 @@ export default function Root() {
          <aside className="flex items-center justify-between col-span-2 px-4 py-3 bg-gradient-to-r from-accent-1 to-accent-2">
             <p className="pl-3">
               <span className="block text-base tracking-wider ">
-                We are M. Our mission is to provide everyone the highest quality courses in the world!
+                We are M. Our mission is to provide everyone the highest quality courses possible in the world!
               </span>
               <span className="text-base">Sign up to gain access to some our courses for free! No bank cards required.  </span>
             </p>
