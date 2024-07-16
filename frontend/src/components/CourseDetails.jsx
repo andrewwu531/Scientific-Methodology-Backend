bb5896731db1d47cf303b96d9300cd4581154f8f
unknown <andrewwu0531>
2024-07-16 18:24:39 +0100

    Course Details - Top 3 Images

--- frontend/src/components/CourseDetails.jsx
+++ frontend/src/components/CourseDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import NavBar from "../components/Navbar";
 
 export default function CourseDetail() {
   const { courseUrl } = useParams();
@@ -65,76 +66,108 @@ export default function CourseDetail() {
   }, {});
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <img
-          src={`${backendURL}${course.course_banner}`}
-          alt={course.course_title}
-          className="object-cover object-top w-[56vw] transition-transform duration-300 h-[60vh] group-hover:scale-105"
-        />
-        <div className="w-[44vw] text-4xl font-bold pt-[8vh] pl-[5vw]">
-          {course.course_title}
-          <p className="text-xl font-medium text-neutral-400">
-            {course.course_category}
-          </p>
-        </div>
+    <div className="bg-black">
+      <div className="fixed top-0 z-30 w-full ">
+        <NavBar />
       </div>
+      <div className="flex flex-col pt-[13.5vh] pb-[8vh]">
+        <div className="relative flex justify-center w-full mb-4">
+          <div className="absolute left-0 z-50 w-full h-full bg-opacity-100 top-5 bg-gradient-to-b from-transparent via-transparent to-black"></div>
 
-      <div className="mx-[18vw] mt-[10vh]">
-        {Object.keys(seriesGroupedVideos).map((seriesName, index) => (
-          <div key={seriesName} className="mb-14">
-            <h3 className="flex items-center justify-center mb-4 text-2xl font-bold">
-              Series {index + 1}: {seriesName}
-            </h3>
-            {seriesGroupedVideos[seriesName].map((video) => (
-              <div key={video.pk}>
-                <div
-                  onClick={() => handleVideoClick(video, seriesName)}
-                  className={`p-2 mb-2 rounded-lg cursor-pointer ${
-                    openAccordion === `${seriesName}-${video.pk}`
-                      ? "bg-purple-900"
-                      : currentVideo.pk === video.pk
-                        ? "bg-purple-900"
-                        : "bg-neutral-900 hover:bg-neutral-700"
-                  }`}
-                >
-                  <div className="flex flex-row items-center justify-between py-2">
-                    <div className="flex flex-row items-center ml-[1vw] space-x-4">
-                      <img
-                        src={`${backendURL}${video.video_icon}`}
-                        alt={course.course_title}
-                        className="object-cover object-top w-8 h-8 ml-3 transition-transform duration-300 rounded-full group-hover:scale-105"
-                      />
-                      <p className="font-sans text-small">
-                        {video.video_title}
-                      </p>
-                    </div>
-                    <div className="mr-[2vw]">
-                      <p className="text-sm">{video.video_duration}</p>
-                    </div>
-                  </div>
-                </div>
-                {openAccordion === `${seriesName}-${video.pk}` && (
-                  <div className="p-2 mt-4 rounded-lg bg-neutral-800">
-                    <div className="relative h-100">
-                      <video
-                        key={currentVideo.pk} // Adding key to ensure video component re-renders
-                        controls
-                        className="object-cover w-full h-full rounded-lg"
-                      >
-                        <source
-                          src={`${backendURL}${currentVideo.video_video}`}
-                          type="video/mp4"
-                        />
-                        Your browser does not support the video tag.
-                      </video>
-                    </div>
-                  </div>
-                )}
-              </div>
-            ))}
+          <div className="relative z-10 w-[24vw] h-[70vh] overflow-hidden">
+            <img
+              src={`${backendURL}/${course.course_banner}`}
+              alt={course.course_title}
+              className="ml-10 object-cover w-full h-full mt-[12vh] rounded-xl transition-transform duration-300 "
+            />
           </div>
-        ))}
+          <div className="relative z-20 w-[33vw] h-[70vh] overflow-hidden">
+            <img
+              src={`${backendURL}/${course.course_banner}`}
+              alt={course.course_title}
+              className="object-cover w-full h-full transition-transform duration-300 rounded-t-xl "
+            />
+          </div>
+          <div
+            className="relative z-10 w-[24vw] h-[70vh] overflow-hidden"
+            style={{ marginLeft: "-1vw" }}
+          >
+            <img
+              src={`${backendURL}/${course.course_banner}`}
+              alt={course.course_title}
+              className="object-cover w-full h-full mt-[12vh] rounded-xl transition-transform duration-300 "
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center w-full px-[10vw]">
+          <div className="flex flex-col space-y-2 w-[20vw] text-center text-neutral-200">
+            <h2 className="mb-8 text-3xl font-bold">{course.course_title}</h2>
+            <p className="font-bold text-md pb-[1vh] ">
+              {course.course_author}
+            </p>
+            <button className="py-2 text-xl text-black bg-purple-800 rounded-3xl">
+              ▶
+            </button>
+          </div>
+        </div>
+
+        <div className="mx-[18vw] mt-[12vh]">
+          {Object.keys(seriesGroupedVideos).map((seriesName, index) => (
+            <div key={seriesName} className="mb-14">
+              <h3 className="flex items-center justify-center mb-4 text-2xl font-bold text-neutral-200">
+                Series {index + 1}: {seriesName}
+              </h3>
+              {seriesGroupedVideos[seriesName].map((video) => (
+                <div key={video.pk}>
+                  <div
+                    onClick={() => handleVideoClick(video, seriesName)}
+                    className={`p-2 mb-2.5 rounded-lg cursor-pointer ${
+                      openAccordion === `${seriesName}-${video.pk}`
+                        ? "bg-purple-900"
+                        : currentVideo.pk === video.pk
+                          ? "bg-purple-900"
+                          : "bg-neutral-950 hover:bg-neutral-700"
+                    }`}
+                  >
+                    <div className="flex flex-row items-center justify-between py-2">
+                      <div className="flex flex-row items-center ml-[1vw] space-x-4">
+                        <img
+                          src={`${backendURL}${video.video_icon}`}
+                          alt={course.course_title}
+                          className="object-cover object-top w-8 h-8 ml-3 transition-transform duration-300 rounded-full group-hover:scale-105"
+                        />
+                        <p className="font-sans text-small">
+                          {video.video_title}
+                        </p>
+                      </div>
+                      <div className="mr-[2vw]">
+                        <p className="text-sm">{video.video_duration}</p>
+                      </div>
+                    </div>
+                  </div>
+                  {openAccordion === `${seriesName}-${video.pk}` && (
+                    <div className="p-2 mt-4 rounded-lg bg-neutral-800">
+                      <div className="relative h-100">
+                        <video
+                          key={currentVideo.pk} // Adding key to ensure video component re-renders
+                          controls
+                          className="object-cover w-full h-full rounded-lg"
+                        >
+                          <source
+                            src={`${backendURL}${currentVideo.video_video}`}
+                            type="video/mp4"
+                          />
+                          Your browser does not support the video tag.
+                        </video>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
